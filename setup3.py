07f727e7396130a2d3fec751868fa09ab8982740
Riley Hales <rileyhales1@gmail.com>
2022-08-20 13:49:24 -0600

    create tethys3 backwards compatibility

---
+++ setup3.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_namespace_packages
+from tethys_apps.app_installation import find_resource_files
+
+# -- Apps Definition -- #
+app_package = 'nitrates'
+release_package = 'tethysapp-' + app_package
+
+# -- Python Dependencies -- #
+dependencies = []
+
+# -- Get Resource File -- #
+resource_files = find_resource_files('tethysapp/' + app_package + '/templates', 'tethysapp/' + app_package)
+resource_files += find_resource_files('tethysapp/' + app_package + '/public', 'tethysapp/' + app_package)
+
+
+setup(
+    name=release_package,
+    version='0.1.0',
+    description='first_test',
+    long_description='',
+    keywords='',
+    author='',
+    author_email='',
+    url='',
+    license='',
+    packages=find_namespace_packages(),
+    package_data={'': resource_files},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=dependencies,
+)
